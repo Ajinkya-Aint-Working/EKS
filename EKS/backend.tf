225e69b381a5f5b730b9227053222fec56738452
Ajinkya-A3 <ajinkyaa3xd@gmail.com>
2026-04-02 09:45:28 +0000

    fixed name

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-state-bucket-951907775476-ap-south-1-an "
+    bucket       = "terraform-state-bucket-951907775476-ap-south-1-an"
     key          = "terraform.tfstate"
     region       = "ap-south-1"
     encrypt      = true
