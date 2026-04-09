f8635ad2b06f8852730a99bda4093e10fffe173d
Ajinkya-A3 <ajinkyaa3xd@gmail.com>
2026-04-09 14:19:55 +0530

    changed the backend configurations

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-state-bucket-951907775476-ap-south-1-an"
-    key          = "terraform.tfstate"
+    bucket       = "terraform-s3-state-007"
+    key          = "Infra/terraform.tfstate"
     region       = "ap-south-1"
     encrypt      = true
     use_lockfile = true
